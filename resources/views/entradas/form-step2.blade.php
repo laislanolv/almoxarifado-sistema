8cd11ed42796d9a6cfc52bd131400d34ca8637d4
Laislan Oliveira <laislan.olv@gmail.com>
2018-01-27 17:32:28 -0300

    Correções

--- resources/views/entradas/form-step2.blade.php
+++ resources/views/entradas/form-step2.blade.php
@@ -62,7 +62,6 @@
                         <td>
                             {!! Form::open(['id' => 'form_excluir_' . $item->pivot->id, 'method' => 'delete', 'route' => ['entradas.del-item.destroy', $entrada->id], 'style'=>'display: inline']) !!}
                             {!! Form::hidden('id_entrada_produto', $item->pivot->id) !!}
-                            {!! Form::hidden('id_produto', $item->pivot->id_produto) !!}
                             {!! Form::button('<i class="fa fa-trash"></i>', ['class' => 'btn btn-danger modal-excluir', 'style' => 'padding: 1px 6px;']) !!}
                             {!! Form::close() !!}
                         </td>
